2884fbf2c6cb89611582f093215b842a3a27c256
sharonalt44 <sharonalt44@gmail.com-GitHub>
2026-02-08 20:00:27 +0200

    Define likes paths as constants

--- libcaf/libcaf/repository.py
+++ libcaf/libcaf/repository.py
@@ -11,7 +11,7 @@ from typing import Concatenate
 
 from . import Blob, Commit, Tree, TreeRecord, TreeRecordType
 from .constants import (DEFAULT_BRANCH, DEFAULT_REPO_DIR, HASH_CHARSET, HASH_LENGTH, HEADS_DIR, HEAD_FILE,
-                        OBJECTS_SUBDIR, REFS_DIR)
+                        OBJECTS_SUBDIR, REFS_DIR, LIKES_BY_USER_DIR, LIKES_BY_COMMIT_DIR, LIKES_PENDING_DIR)
 from .plumbing import hash_object, load_commit, load_tree, save_commit, save_file_content, save_tree
 from .ref import HashRef, Ref, RefError, SymRef, read_ref, write_ref
 from . import likes
@@ -567,9 +567,9 @@ class Repository:
 
         return likes.create_like(
             objects_dir=self.objects_dir(),
-            likes_by_user_dir=self.refs_dir() / "likes" / "by-user",
-            likes_by_commit_dir=self.refs_dir() / "likes" / "by-commit",
-            likes_pending_dir=self.refs_dir() / "likes" / "pending",
+            likes_by_user_dir=self.refs_dir() / LIKES_BY_USER_DIR,
+            likes_by_commit_dir=self.refs_dir() / LIKES_BY_COMMIT_DIR,
+            likes_pending_dir=self.refs_dir() / LIKES_PENDING_DIR,
             commit_hash=commit_hash,
             user=user,
         )
